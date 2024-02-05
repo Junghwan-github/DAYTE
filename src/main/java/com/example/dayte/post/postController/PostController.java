860fe19b18c2e60de09000f87792e4db84afa091
JUNGIE <j2studio.jungie2@gmail.com>
2024-02-06 04:30:34 +0900

    포스트 리스트 수정중 서머노트 이미지 업로드 기능 구현

--- src/main/java/com/example/dayte/post/postController/PostController.java
+++ src/main/java/com/example/dayte/post/postController/PostController.java
@@ -96,7 +96,7 @@ public class PostController {
         model.addAttribute("postList", postService.getPostList(pageable));
         //
 
-        Page<Post> postListPage =postService.getPostList(pageable);
+        Page<Post> postListPage = postService.getPostList(pageable);
         int postTotalPage = postListPage.getTotalPages();
 
         int nowPage = postListPage.getNumber();
@@ -141,56 +141,7 @@ public class PostController {
     // 포스트 이미지 등록 로직 수행
     @PostMapping("/uploadSummernoteImageFile")
     @ResponseBody
-    public ResponseEntity<Map<String, String>> uploadSummernoteImageFile(@RequestParam("files") MultipartFile multipartFile) {
-        Map<String, String> resultMap = new HashMap<>();
-        System.out.println("ddddddddddasdfasdfasdf" + multipartFile);
-        /*JsonObject jsonObject = new JsonObject();*/
-
-        String fileRoot = "\\\\192.168.10.75/temp/images/post/";	//저장될 파일 경로
-        String originalFileName = multipartFile.getOriginalFilename();	//오리지날 파일명
-        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));	//파일 확장자
-        String savedFileName = UUID.randomUUID() + extension;
-
-
-        File targetFile = new File(fileRoot + savedFileName);
-
-        try {
-//            InputStream fileStream = multipartFile.getInputStream();
-            multipartFile.transferTo(targetFile);
-//            FileUtils.copyInputStreamToFile(fileStream, targetFile);	//파일 저장
-
-            String imageUrl = "/temp/images/post/" + savedFileName;
-
-            resultMap.put("url", imageUrl);
-            resultMap.put("responseCode", "success");
-            return ResponseEntity.ok(resultMap);
-
-        } catch (IOException e) {
-            if (targetFile.exists()) {
-                targetFile.delete();
-            }
-
-//            FileUtils.deleteQuietly(targetFile);	// 실패시 저장된 파일 삭제
-
-            /*jsonObject.addProperty("responseCode", "error");*/
-
-            resultMap.put("responseCode", "error");
-//
-//            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(resultMap);
-        }
-
+    public ResponseEntity<Map<String, String>> uploadImage(@RequestParam("files") MultipartFile multipartFile) {
+        return postService.uploadImage(multipartFile);
     }
-    @GetMapping("/summernoteImage/{fileName:.+}")
-    public ResponseEntity<File> getSummernoteImage(@PathVariable String fileName) {
-        String fileRoot = "/temp/images/post/";
-        File file = new File(fileRoot + fileName);
-
-        if (file.exists()) {
-            return ResponseEntity.ok(file);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
 }
