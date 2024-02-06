50012c4cc1bf421ab01e107b4673392dc5240faf
sunyoungkimkk <sunyoung941127@gmail.com>
2024-02-06 17:26:53 +0900

    내가 쓴 글 List 뽑아서 view에 보이게
    address : /myReview

--- src/main/java/com/example/dayte/post/postRepository/PostRepository.java
+++ src/main/java/com/example/dayte/post/postRepository/PostRepository.java
@@ -1,13 +1,14 @@
 package com.example.dayte.post.postRepository;
 
 
+import com.example.dayte.members.domain.User;
 import com.example.dayte.post.domin.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
-
+    Page<Post> findAllByUser(User user, Pageable pageable);
 
 }
